54bf4bce42b63d54de0f0e55e80924637f996459
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2023-01-06 14:44:30 -0300

    chore: add some useful remaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,3 +9,11 @@ keyset('n', '<space>', ':nohlsearch<CR>', opts) -- Space remove highlight serch
 keyset('n', '[g', ':Gitsigns next_hunk<CR>', opts) -- Next git change
 keyset('n', ']g', ':Gitsigns prev_hunk<CR>', opts) -- Prev git change
 keyset('n', '<leader>d', ':Gitsigns diffthis<CR>', opts)
+
+-- Keep cursor in the middle of the screen when jump with Ctrl+(d|u)
+keyset("n", "<C-d>", "<C-d>zz")
+keyset("n", "<C-u>", "<C-u>zz")
+
+-- Keep cursor in the middle of the screen when serching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
