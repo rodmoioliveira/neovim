d5a50f7a60a64cf9e5fce54c2135ec81d7081ef9
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2023-01-06 17:33:05 -0300

    chore: add plugins.better-whitespace

--- init.lua
+++ init.lua
@@ -7,6 +7,7 @@ require("search")
 require("plugins.coc")
 require("plugins.git")
 require("plugins.ale")
+require("plugins.better-whitespace")
 
 vim.cmd('colorscheme bow-wob')
 vim.cmd('highlight SignColumn guibg=bow-wob')
---
+++ lua/plugins/better-whitespace.lua
@@ -0,0 +1,2 @@
+vim.g.strip_whitespace_on_save = true
+vim.g.strip_whitespace_confirm = false
