ec64eb28e01364b543c8a4536761c48a234f2316
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2023-01-09 21:09:41 -0300

    feat: add folke/which-key.nvim

--- lua/my_packer.lua
+++ lua/my_packer.lua
@@ -94,4 +94,15 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = {'kyazdani42/nvim-web-devicons', opt = true}
     }
+
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
 end)
