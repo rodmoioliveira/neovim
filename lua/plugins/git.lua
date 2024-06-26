79920c7c242a711f78b66aab1b698fdf67c26314
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2024-06-26 07:51:36 -0300

    fix: update gitsigns config
    Signed-off-by: Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,41 +1,11 @@
 require('gitsigns').setup {
   signs = {
-    add = {
-      hl = 'GitSignsAdd',
-      text = '+',
-      numhl = 'GitSignsAddNr',
-      linehl = 'GitSignsAddLn'
-    },
-    change = {
-      hl = 'GitSignsChange',
-      text = '~',
-      numhl = 'GitSignsChangeNr',
-      linehl = 'GitSignsChangeLn'
-    },
-    delete = {
-      hl = 'GitSignsDelete',
-      text = '-',
-      numhl = 'GitSignsDeleteNr',
-      linehl = 'GitSignsDeleteLn'
-    },
-    topdelete = {
-      hl = 'GitSignsDelete',
-      text = '-',
-      numhl = 'GitSignsDeleteNr',
-      linehl = 'GitSignsDeleteLn'
-    },
-    changedelete = {
-      hl = 'GitSignsChange',
-      text = '~',
-      numhl = 'GitSignsChangeNr',
-      linehl = 'GitSignsChangeLn'
-    },
-    untracked = {
-      hl = 'GitSignsAdd',
-      text = '┆',
-      numhl = 'GitSignsAddNr',
-      linehl = 'GitSignsAddLn'
-    }
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '-' },
+    topdelete = { text = '-' },
+    changedelete = { text = '~' },
+    untracked = { text = '┆' }
   },
   signcolumn = false, -- Toggle with `:Gitsigns toggle_signs`
   numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
@@ -62,8 +32,7 @@ require('gitsigns').setup {
     relative = 'cursor',
     row = 0,
     col = 2
-  },
-  yadm = { enable = false }
+  }
 }
 
 -- https://vim.fandom.com/wiki/Xterm256_color_names_for_console_Vim
