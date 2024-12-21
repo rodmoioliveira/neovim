7f34d472dbbc409ba1479e054c4db272e6b79f27
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2024-12-21 17:21:58 -0300

    feat: Add lualine configs
    Signed-off-by: Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>

--- init.lua
+++ init.lua
@@ -205,7 +205,45 @@ require("lazy").setup({
     end,
   },
   { "L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp" },
-  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }, opts = {}, config = function() require("lualine").setup() end },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup {
+        options = {
+          icons_enabled = false,
+          theme = 'codedark',
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+          disabled_filetypes = { statusline = {}, winbar = {} },
+          ignore_focus = {},
+          always_divide_middle = true,
+          globalstatus = false,
+          refresh = { statusline = 1000, tabline = 1000, winbar = 1000 }
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' }
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {}
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {}
+      }
+    end
+  },
   { "rasulomaroff/cmp-bufname" },
   { "hrsh7th/cmp-buffer" },
   { "hrsh7th/cmp-cmdline" },
