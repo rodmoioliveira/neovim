4c22329a41da164af19b646bd210f2ecc50ad5c3
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2024-12-21 10:36:13 -0300

    feat: Remove old plugins
    Signed-off-by: Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,17 +1,12 @@
 return {
   { "lukas-reineke/lsp-format.nvim", opt = {} },
-  { "djoshea/vim-autoread", opt = {} },
   { "ibhagwan/fzf-lua", dependencies = { "nvim-tree/nvim-web-devicons" }, config = function() require("fzf-lua").setup({}) end },
   { "lewis6991/gitsigns.nvim", opt = {}, config = function() require("gitsigns").setup({}) end },
-  { "lewis6991/impatient.nvim", ops = {} },
-  { "lukas-reineke/indent-blankline.nvim", opt = {} },
   { "m4xshen/hardtime.nvim", dependencies = { "MunifTanjim/nui.nvim" }, opts = {} },
-  { "norcalli/nvim-colorizer.lua", opt = {} },
   { "ntpeters/vim-better-whitespace", opt = {} },
   { "p7g/vim-bow-wob", opt = {} },
   { "tpope/vim-commentary", opt = {} },
   { "tpope/vim-surround", opt = {} },
-  { "wbthomason/packer.nvim", opt = {} },
   {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
